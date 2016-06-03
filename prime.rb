5d79e9328a253e9d56d72a5e64f02a53126aa7d8
grahk93 <grace.juster@flatironschool.com>
2016-06-03 11:47:47 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+
+
+def prime?(number)
+  if number <= 1 #prime cant be less than or equal to one
+    return false
+  end
+  if number%2 == 0 #prime cant be even
+    return false 
+  end
+   if number%1 == 0 && number%number == 0
+    return true
+  else 
+    return false
+  end
+end
+#THIS IS WRONG
+#NEED A DIVISOR NEED TO MOVE ON 
